2fbfe7a84fa3e4a949f0dc884dd45eb0a39f9d75
Jan Lehigh <janlehigh461437@gmail.com>
2024-10-05 08:29:38 -0600

    Add the revised Subscriptions migration with currency set up and default for status

---
+++ db/migrate/20241004203014_create_subscriptions.rb
@@ -0,0 +1,14 @@
+class CreateSubscriptions < ActiveRecord::Migration[7.1]
+  def change
+    create_table :subscriptions do |t|
+      t.references :customer, null: false, foreign_key: true
+      t.references :tea, null: false, foreign_key: true
+      t.string :title
+      t.decimal :price, precision: 8, scale: 2 #currency set up 
+      t.string :status, null: false, default: 'active'
+      t.string :frequency, null: 
+
+      t.timestamps
+    end
+  end
+end
