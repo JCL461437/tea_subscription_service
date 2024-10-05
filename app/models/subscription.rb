3368ff1819c0d94a92e1cc02065e81144629c54d
Jan Lehigh <janlehigh461437@gmail.com>
2024-10-05 17:24:27 -0600

    Add instance method to subscription model for changing status

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -6,4 +6,12 @@ class Subscription < ApplicationRecord
   validates :price, presence: true, numericality: true 
   validates :status, presence: true
   validates :frequency, presence: true
+
+  def change_status
+    if self.status == "active"
+      self.status == "cancelled"
+    elsif self.status == "cancelled"
+      self.status == "active"
+    end
+  end
 end
