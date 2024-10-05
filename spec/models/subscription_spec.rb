2e33f50ba625c45b66a33906bf27a3764ea7b239
Jan Lehigh <janlehigh461437@gmail.com>
2024-10-05 17:24:06 -0600

    Add test for new model instance method for change_status

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -12,4 +12,24 @@ RSpec.describe Subscription, type: :model do
     it { should validate_presence_of(:status) }
     it { should validate_presence_of(:frequency) }
   end
+
+  describe "model methods" do
+    describe "instance methods" do
+      describe ".change_status" do
+        it "will change the status to 'cancelled' or 'active' depending on the exisiting status" do
+          customer1 = Customer.create!(first_name: 'Billy', last_name: 'Johnson', email: 'billy@example.com', address: '123 Maple Street, Billyland, UT')
+          customer2 = Customer.create!(first_name: 'Bob', last_name: 'Smith', email: 'bob@example.com', address: '456 Oak Avenue, Bobtopia, NY')
+          tea1 = Tea.create!(title: 'Earl Grey', description: 'A fragrant black tea flavored with bergamot.', temperature: 200, brew_time: 4)
+          subscription1 = Subscription.create!(customer: customer1, tea: tea, title: 'Earl Grey Subscription', price: 10.00, status: 'active', frequency: 'monthly')
+          subscription2 = Subscription.create!(customer: customer2, tea: tea, title: 'Green Tea Subscription', price: 14.00, status: 'cancelled', frequency: 'weekly')
+
+          expect(subscription1.status).to eq('active')
+          expect(subscription1.change_status).to eq('cancelled')
+
+          expect(subscription1.status).to eq('cancelled')
+          expect(subscription1.change_status).to eq('active')
+        end
+      end
+    end
+  end
 end
