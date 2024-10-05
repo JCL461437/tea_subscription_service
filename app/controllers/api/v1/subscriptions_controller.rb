4b2982c2760b0973abdcac14ecac76ea01932490
Jan Lehigh <janlehigh461437@gmail.com>
2024-10-05 15:24:38 -0600

    Add the subscriptions_controller.rb file with the create action

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,3 +1,15 @@
 class Api::V1::SubscriptionsController < ApplicationController
 
+  def create
+    customer = Customer.find(params[:customer_id])
+    tea = Tea.find(params[:tea_id])
+
+    subscription = Subscription.create(customer, tea)
+
+    if subscription.save
+      render json: SubscriptionSerializer.new.new_subscription(customer, tea, subscription), status: 200
+    else 
+      render json: {error: 'Subscription attempt failed, try re-entering your customer email and tea name'}, status: 422
+    end
+  end
 end
