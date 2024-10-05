cebd70e9e6c2e70da33f87ab33a1ca652fdcf646
Jan Lehigh <janlehigh461437@gmail.com>
2024-10-05 09:04:47 -0600

    Chang the subscriptions migration to remove unecessary null:false syntax

--- db/migrate/20241004203014_create_subscriptions.rb
+++ db/migrate/20241004203014_create_subscriptions.rb
@@ -5,8 +5,8 @@ class CreateSubscriptions < ActiveRecord::Migration[7.1]
       t.references :tea, null: false, foreign_key: true
       t.string :title
       t.decimal :price, precision: 8, scale: 2 #currency set up 
-      t.string :status, null: false, default: 'active'
-      t.string :frequency, null: false
+      t.string :status default: 'active'
+      t.string :frequency
 
       t.timestamps
     end
