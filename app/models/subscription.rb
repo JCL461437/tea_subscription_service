8736ab832c1938860bb18db90811e16d6a2af5cc
Jan Lehigh <janlehigh461437@gmail.com>
2024-10-05 15:32:58 -0600

    Remove validations for unique title which is not necessary

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,7 +2,7 @@ class Subscription < ApplicationRecord
   belongs_to :customer
   belongs_to :tea
 
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true
   validates :price, presence: true, numericality: true 
   validates :status, presence: true
   validates :frequency, presence: true
