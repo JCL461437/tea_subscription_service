257cd58e687e008c03a55865b3d3b321ba629aa8
Jan Lehigh <janlehigh461437@gmail.com>
2024-10-05 16:13:56 -0600

    Revised flow control and calling of the subscription_serializer.rb file

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,23 +1,29 @@
 class Api::V1::SubscriptionsController < ApplicationController
 
   def create
-    customer = Customer.where(email: params[:customer])
-    tea = Tea.where(title: params[:tea])
+    customer = Customer.find_by(email: params[:customer])
+    tea = Tea.find_by(title: params[:tea])
     frequency = params[:frequency]
-    title = "Subscription for #{tea} tea."
+    title = "Subscription for #{tea.title} tea."
 
-    subscription = Subscription.create(
+    unless customer && tea
+      render json: { error: 'Customer or Tea not found' }, status: 404
+      return
+    end
+
+    subscription = Subscription.new(
       customer: customer,
       tea: tea,
       title: title,
       price: 2.0,
-      frequency: frequency
-      )
+      frequency: frequency,
+      status: "active"  
+    )
 
     if subscription.save
-      render json: SubscriptionSerializer.new.new_subscription(customer, tea, subscription), status: 200
-    else 
-      render json: {error: 'Subscription attempt failed, try re-entering your customer email and tea name'}, status: 422
+      render json: SubscriptionSerializer.new(subscription)
+    else
+      render json: { error: 'Subscription attempt failed, try re-entering your customer email and tea name' }, status: 422
     end
   end
 end
